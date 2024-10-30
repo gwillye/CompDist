1c057d8a3e93c5e69cb53dd3e8641ff43bc5c71a
gwillye <gwillyestudante@gmail.com>
2024-10-29 20:02:50 -0400

    Dockerfile e docker-compose criado

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 from flask_httpauth import HTTPBasicAuth
 from app.config import Config
 import logging
+import os
 
 # Configuração de logs
 logging.basicConfig(filename="log/app.log", level=logging.INFO)
@@ -23,8 +24,29 @@ def create_app():
     with app.app_context():
         # Importa os módulos necessários
         from app import models, views, admin  
-        
+
         # Cria o banco de dados, se não existir
         db.create_all()
+
+        # Cria o usuário administrativo padrão, se não existir
+        create_admin_user()
     
     return app
+
+from app.services.auth_service import hash_password
+from app.models import Profile
+
+def create_admin_user():
+    admin_username = os.environ.get("ADMIN_USERNAME", "admin")
+    admin_password = os.environ.get("ADMIN_PASSWORD", "admin_password")
+    admin_email = os.environ.get("ADMIN_EMAIL", "admin@example.com")
+
+    # Verifica se o usuário administrador já existe
+    if not Profile.query.filter_by(username=admin_username).first():
+        user = Profile(
+            username=admin_username,
+            password=hash_password(admin_password),
+            email=admin_email
+        )
+        db.session.add(user)
+        db.session.commit()
--- app/config.py
+++ app/config.py
@@ -1,8 +1,18 @@
-import os
+'''import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/usersdb.sqlite3'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BASIC_AUTH_FORCE = True
+    FLASK_ADMIN_SWATCH = 'yeti'
+'''
+
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'default-secret-key')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///instance/usersdb.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BASIC_AUTH_FORCE = True
     FLASK_ADMIN_SWATCH = 'yeti'
